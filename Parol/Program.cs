563ecf88e208a0dc0671350e1b3beec4e090d778
Ali <aliaea@code.edu.az>
2024-04-09 13:27:58 +0400

    first commit

---
+++ Parol/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Parol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user = new User("User1", "Password1");
+            Console.WriteLine("Account ugurla acildi");
+
+        }
+    }
+}
---
+++ Parol/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parol
+{
+    internal class User
+    {
+        public string UserName { get; set; }
+        private string Password { get; set; }
+
+        public User(string UserName, string Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+        public void SetPassword(string newPassword)
+        {
+            if (!DuzgunSifre(newPassword))
+            {
+                Console.WriteLine("Daha duzgun sifre girin.Minimum 8 karakter isledin");
+            }
+            Password = newPassword;
+            Console.WriteLine("Şifre ugurla deyisdirildi");
+            return;
+        }
+            public bool CheckPassword(string password)
+        {
+            return Password == password;
+        }
+        private bool DuzgunSifre(string password)
+        {
+            return password.Length >= 8;
+        }
+    }
+}
